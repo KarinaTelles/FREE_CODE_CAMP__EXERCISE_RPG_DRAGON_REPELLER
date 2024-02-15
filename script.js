50003fafe7f1924882d5502ee72c58faa9422ecc
Karina <karina@gmail.com>
2024-02-15 20:38:59 -0300

    magic!

--- script.js
+++ script.js
@@ -18,6 +18,8 @@ const monsterHealthText = document.querySelector("#monsterHealth");
 function goStore() {
   const button1 = document.querySelector("#button1");
   button1.innerText = "Buy 10 health (10 gold)";
+  button2.innerText = "Buy weapon (30 gold)";
+  button3.innerText = "Go to town square";
 }
 function goCave(){
   console.log("Going to cave.");
