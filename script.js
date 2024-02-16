270a62c536137ed484b6e0b928a632432eec9a4f
Karina <karina@gmail.com>
2024-02-16 11:22:49 -0300

    add the fucntions that were missing

--- script.js
+++ script.js
@@ -15,6 +15,12 @@ const goldText = document.querySelector("#goldText");
 const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
+
+//initialize buttons
+button1.onclick = goStore;
+button2.onclick = goCave;
+button3.onclick = fightDragon;
+
 function goStore() {
   button1.innerText = "Buy 10 health (10 gold)";
   button2.innerText = "Buy weapon (30 gold)";
@@ -25,13 +31,19 @@ function goStore() {
   button2.onclick = buyWeapon;
   button3.onclick = goTown;
 }
+
 function goCave(){
   console.log("Going to cave.");
 }
 function fightDragon(){
   console.log("Fighting dragon.");
 }
-//initialize buttons
-button1.onclick = goStore;
-button2.onclick = goCave;
-button3.onclick = fightDragon;
+function buyHealth(){
+
+}
+function buyWeapon(){
+
+}
+function goTown(){
+  
+}
