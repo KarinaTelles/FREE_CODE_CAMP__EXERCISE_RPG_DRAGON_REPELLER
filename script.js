3de3586ecd48313eb4ce7fe8f298071046d3d94f
Karina <karina@gmail.com>
2024-02-15 21:03:07 -0300

    You enter the store.

--- script.js
+++ script.js
@@ -16,10 +16,14 @@ const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
 function goStore() {
-  const button1 = document.querySelector("#button1");
   button1.innerText = "Buy 10 health (10 gold)";
   button2.innerText = "Buy weapon (30 gold)";
   button3.innerText = "Go to town square";
+  const text = document.querySelector("#text");
+  text.innerText = "You enter the store.";
+  button1.onclick = buyHealth;
+  button2.onclick = buyWeapon;
+  button3.onclick = goTown;
 }
 function goCave(){
   console.log("Going to cave.");
