d712c9742fcec68b7bfd3f2abea2d8a7266f6af4
Karina <karina@gmail.com>
2024-02-17 19:42:32 -0300

    que medo .-.

--- script.js
+++ script.js
@@ -35,9 +35,7 @@ button1.onclick = goStore;
 button2.onclick = goCave;
 button3.onclick = fightDragon;
 
-function update(location){}
-
-function goTown() {
+function update(location){
   button1.innerText = "Go to store";
   button2.innerText = "Go to cave";
   button3.innerText = "Fight dragon";
@@ -47,16 +45,11 @@ function goTown() {
   text.innerText = "You are in the town square. You see a sign that says \"Store\".";
 }
 
-function goStore() {
-  button1.innerText = "Buy 10 health (10 gold)";
-  button2.innerText = "Buy weapon (30 gold)";
-  button3.innerText = "Go to town square";
-  const text = document.querySelector("#text");
-  text.innerText = "You enter the store.";
-  button1.onclick = buyHealth;
-  button2.onclick = buyWeapon;
-  button3.onclick = goTown;
+function goTown() {
+  update();
 }
+
+function goStore() {}
 
 function goCave(){
   console.log("Going to cave.");
