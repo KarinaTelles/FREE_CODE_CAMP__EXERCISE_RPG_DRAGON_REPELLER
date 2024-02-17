832caad05d31613aa425920862c264df5a69fc30
Karina <karina@gmail.com>
2024-02-17 04:25:09 -0300

    add texts to my array of button text

--- script.js
+++ script.js
@@ -17,7 +17,8 @@ const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
 const locations = [
   {
-    name: "town square"
+    name: "town square",
+    "button text": ["Go to store","Go to cave","Fight dragon"]
   }
 ];
 
