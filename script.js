51cc667b446c99aa456aaca1e12548783de21dbd
Karina <karina@gmail.com>
2024-02-20 14:58:00 -0300

    What is a if without an else?
    It's like Romeo without Julliet

--- script.js
+++ script.js
@@ -67,11 +67,13 @@ function fightDragon(){
   console.log("Fighting dragon.");
 }
 function buyHealth() {
-  if(gold >= 10){
+  if (gold >= 10) {
     gold -= 10;
     health += 10;
     goldText.innerText = gold;
     healthText.innerText = health;
+  } else{
+    
   }
 }
 function buyWeapon(){
