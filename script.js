e18dbb97d952e7204582fa5c4404fd7ce7fd94c7
Karina <karina@gmail.com>
2024-02-24 22:48:17 -0300

    commit de segurança

--- script.js
+++ script.js
@@ -75,7 +75,13 @@ const locations = [
     "button text": ["REPLAY?","REPLAY?","REPLAY?"],
     "button functions":[restart,restart,restart],
     text: "You die. &#x2620;"
-  }
+  }, 
+  {
+    name: "win",
+    "button text": ["REPLAY?", "REPLAY?", "REPLAY?"],
+    "button functions": [restart, restart, restart],
+    text: "You defeat the dragon! YOU WIN THE GAME! &#x1F389;"
+  },
 ];
 
 // initialize buttons
@@ -91,7 +97,7 @@ function update(location) {
   button1.onclick = location["button functions"][0];
   button2.onclick = location["button functions"][1];
   button3.onclick = location["button functions"][2];
-  text.innerText = location.text;
+  text.innerHTML = location.text;
 }
 
 function goTown() {
@@ -200,6 +206,9 @@ function defeatMonster() {
 }
 function lose() {
   update(locations[5]);
+}
+function winGame() {
+  update(locations[6]);
 }
 function restart(){
   xp = 0;
