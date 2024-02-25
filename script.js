b430bd2187065a394060cf83c5deee82d3e55b64
Karina <karina@gmail.com>
2024-02-24 23:18:30 -0300

    commit de segurança

--- script.js
+++ script.js
@@ -194,6 +194,9 @@ function attack() {
     
   }
 }
+function getMonsterAttackValue(level){ 
+  
+}
 function dodge() {
   text.innerText = "You dodge the attack from the " + monsters[fighting].name;
 }
