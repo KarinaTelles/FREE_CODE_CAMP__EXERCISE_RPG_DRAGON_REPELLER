a5564e767adfc04188d592253206412b68b9e047
Karina <karina@gmail.com>
2024-02-25 21:14:54 -0300

    commit merge

--- script.js
+++ script.js
@@ -264,5 +264,9 @@ function pick(guess) {
   while (numbers.length < 10) {
     numbers.push(Math.floor(Math.random() * 11));
   }
+  text.innerText = "You picked " + guess + ". Here are the random numbers:\n";
+  for (let i = 0; x < 5; x++) {
+
+  }
 }
 
