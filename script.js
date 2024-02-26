b054ef4b5599174a83be4e8ade985e3b7d82b053
Karina <karina@gmail.com>
2024-02-25 22:14:34 -0300

    commit de segurança

--- script.js
+++ script.js
@@ -265,8 +265,18 @@ function pick(guess) {
     numbers.push(Math.floor(Math.random() * 11));
   }
   text.innerText = "You picked " + guess + ". Here are the random numbers:\n";
-  for (let i = 0; x < 5; x++) {
-
+  for (let i = 0; i < 10; i++) {
+    text.innerText += numbers[i] + "\n";
+  }
+  if (numbers.includes(guess)) {
+    text.innerText += "Right! You win 20 gold!";
+    gold += 20;
+    goldText.innerText = gold;
+  }
+  else{
+    text.innerText += "Wrong! You lose 10 health!";
+    health -=10;
+    healthText.innerText = health;
   }
 }
 
